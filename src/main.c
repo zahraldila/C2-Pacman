128eacd6cf9fd8867da4723513ecfa493d615154
Qlio Amanda <qliofebriany91@gmail.com>
2025-03-03 08:55:55 +0700

    update main.c

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "game.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int main(int argc, char *argv[])
+{
+    InitGame();
+
+    SDL_Event event;
+    while (1)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            HandleEvent(&event);
+        }
+
+        UpdateGame();
+        RenderGame();
+
+        SDL_Delay(40);
+    }
+
+    return 0;
+}
